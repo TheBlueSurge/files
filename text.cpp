27d61c829809a9ba734c0edff08e527dd8b68d64
TheBlueSurge <TheBlueSurge@users.noreply.github.com>
2018-02-14 14:30:56 -0400

    Added abc.txr file

---
+++ text.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+   
+   ofstream myfile{"abc.txt"};
+	
+	if(myfile)//checks to see if the file opens
+		myfile << a << b << c;
+		
+		else{
+			cout <<" could not open correctly" << endl;
+		}
+	myfile.close();
+ 
+   return 0;
+}
